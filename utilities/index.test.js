ad8238d0ed96600569ba0a02145ccca00c5bf784
amit08255 <amitcute3@gmail.com>
2020-08-11 00:22:54 +0530

    Added test for map utility function.

--- utilities/index.test.js
+++ utilities/index.test.js
@@ -1,4 +1,4 @@
-const { compose } = require('./index');
+const { compose, map } = require('./index');
 
 describe('compose utility', () => {
     test('should return function', () => {
@@ -26,5 +26,32 @@ describe('compose utility', () => {
         const x = 23;
         const incFun = compose(x, increment);
         expect(() => incFun(2)).toThrow(Error);
+    });
+});
+
+describe('map utility', () => {
+    test('should call mapped function on mappable', () => {
+        const increment = (a) => a + 1;
+        const values = [1, 2, 3, 4, 5];
+        const expected = [2, 3, 4, 5, 6];
+        const finalValue = map(increment)(values);
+
+        expect(finalValue).toEqual(expected);
+    });
+
+    test('should through error when value is not valid mappable', () => {
+        const increment = (a) => a + 1;
+        const values = 23;
+        const mapValue = map(increment);
+
+        expect(() => mapValue(values)).toThrow(Error);
+    });
+
+    test('should through error when passed function is not valid function', () => {
+        const increment = 23;
+        const values = [1, 2, 3, 4, 5];
+        const mapValue = map(increment);
+
+        expect(() => mapValue(values)).toThrow(Error);
     });
 });
