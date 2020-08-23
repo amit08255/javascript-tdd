8f22cffe35164c14458e475df4d2f92122b3b8a2
amit08255 <amitcute3@gmail.com>
2020-08-23 21:18:12 +0530

    Updated login component test with event handlers.

--- components/login/index.spec.jsx
+++ components/login/index.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Login from './index';
 
 describe('Login component tests', () => {
@@ -15,6 +15,42 @@ describe('Login component tests', () => {
             />,
         );
 
-        expect(screen.getByText('Submit')).toBeInTheDocument();
+        expect(screen.getByText(/submit/i)).toBeInTheDocument();
+    });
+
+    test('login input event handler works correctly', () => {
+        const username = 'amit';
+        const password = 'pass';
+        const onUsernameChange = jest.fn();
+        const onPasswordChange = jest.fn();
+        const onSubmitClick = jest.fn();
+
+        render(
+            <Login
+                username={' '}
+                password={' '}
+                onUsernameChange={(e) => onUsernameChange(e.target.value)}
+                onPasswordChange={(e) => onPasswordChange(e.target.value)}
+                onSubmit={onSubmitClick}
+            />,
+        );
+
+        fireEvent.change(screen.getByPlaceholderText(/username/i), {
+            target: {
+                value: username,
+            },
+        });
+
+        fireEvent.change(screen.getByPlaceholderText(/password/i), {
+            target: {
+                value: password,
+            },
+        });
+
+        fireEvent.click(screen.getByText(/submit/i));
+
+        expect(onUsernameChange).toHaveBeenCalledWith(username);
+        expect(onPasswordChange).toHaveBeenCalledWith(password);
+        expect(onSubmitClick).toHaveBeenCalled();
     });
 });
